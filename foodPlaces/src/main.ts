33ba64ba17fcc849e807c905c20c1ce1a1f4f7c8
Jonnie <jcarino2003@hotmail.com>
2024-12-12 14:54:21 +0000

    Auth not working

--- foodPlaces/src/main.ts
+++ foodPlaces/src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAuth0({
+      domain: 'dev-tnafsghc1wquofma.us.auth0.com',
+      clientId: 'Ii0Iz28p8KliUBpRe2CI8xaD49hMgUDE',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+    provideHttpClient(),
+    appConfig.providers
+  ]
+}).catch((err) => console.error(err));
